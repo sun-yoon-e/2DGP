71ce3d824328ef1b5fc95b2637dff9d871c08cfc
sun-yoon-e <shinee252525@icloud.com>
2019-10-02 17:38:57 +0900

    Lecture04

--- Lecture04/drill_03.py
+++ Lecture04/drill_03.py
@@ -8,7 +8,7 @@ character = load_image('character.png')
 
 while True:
     x = 400
-    while x < 750 :
+    while x < 750:
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, 90)
@@ -16,21 +16,21 @@ while True:
         delay(0.001)
 
     y = 90
-    while y < 550 :
+    while y < 550:
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x, y)
         y = y + 2
         delay(0.001)
 
-    while x > 30 :
+    while x > 30:
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x, y)
         x = x - 2
         delay(0.001)
 
-    while y > 90 :
+    while y > 90:
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x, y)
