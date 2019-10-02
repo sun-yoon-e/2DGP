2b3eca6227cb9d1792c98e1d3dfabeedb4803815
sun-yoon-e <shinee252525@icloud.com>
2019-10-02 18:33:41 +0900

    실습

--- Lecture06/move_character_with_key.py
+++ Lecture06/move_character_with_key.py
@@ -3,6 +3,35 @@ from pico2d import *
 
 def handle_events():
     # fill here
+    global running
+    #global  x
+    global dir
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        #
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
+        #
+        #elif event.type == SDL_KEYDOWN:
+        #    if event.key == SDLK_RIGHT:
+        #        x = x + 10
+        #    elif event.key == SDLK_LEFT:
+        #        x =  x - 10
+        #    elif event.key == SDLK_ESCAPE:
+        #        running = False
     pass
 
 
@@ -13,6 +42,8 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
+#
+dir = 0
 
 while running:
     clear_canvas()
@@ -22,6 +53,8 @@ while running:
 
     handle_events()
     frame = (frame + 1) % 8
+    #
+    x += dir * 5
 
     delay(0.05)
 
