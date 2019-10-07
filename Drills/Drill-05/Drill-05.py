c70e74d55f5de3fd38ca47d51ca589f0b1962d40
sun-yoon-e <shinee252525@icloud.com>
2019-10-07 19:24:12 +0900

    Drill-05
    새로운 변수로 소스 작성

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -25,24 +25,28 @@ kpu_ground = load_image('KPU_GROUND.png')
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 character_x, character_y, character_d = KPU_WIDTH // 2, KPU_HEIGHT // 2, 3
+end_x = character_x
+end_y = character_y
 frame = 0
 hide_cursor()
 
 hand.draw(x, y)
 character.clip_draw(200, 0, 100, 100, 0, 0)
 
-while True:
+while running:
+    move_x = (end_x - character_x) / 20
+    move_y = (end_y - character_y) / 20
+    character_x += move_x
+    character_y += move_y
+
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     hand.draw(x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
 
-    while running:
-        clear_canvas()
-        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-        hand.draw(x, y)
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-        update_canvas()
-        frame = (frame + 1) % 8
-
-        delay(0.02)
-        handle_events()
+    delay(0.02)
+    handle_events()
 
 close_canvas()
