4d9a75e0a833ab8e947b98c573e3b62b8fd1a495
sun-yoon-e <shinee252525@icloud.com>
2019-10-07 18:55:41 +0900

    Drill-05
    러닝타입 추가
    캔버스명령 추가

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -7,8 +7,11 @@ def handle_events():
     global x, y
     events = get_events()
     for event in events:
-        if event.type == SDL_MOUSEMOTION:
+        if event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            running = True
             x, y = event.x, KPU_HEIGHT -1 - event.y
+        elif event.type == SDL_MOUSEBUTTONUP:
+            running = False
     pass
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -21,13 +24,17 @@ x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
 
-while running:
-    clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+while True:
     hand.draw(x, y)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
+    while running:
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        hand.draw(x, y)
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
 
-    delay(0.02)
-    handle_events()
+        delay(0.02)
+        handle_events()
+
+close_canvas()
