1dfcf8e309abdb777762643a1b5c8a8b449e4396
sun-yoon-e <shinee252525@icloud.com>
2019-10-09 18:12:39 +0900

    실습

--- Lecture/Lecture08/curve.py
+++ Lecture/Lecture08/curve.py
@@ -1,10 +1,8 @@
 import turtle
 import random
 
-
 def stop():
     turtle.bye()
-
 
 def prepare_turtle_canvas():
     turtle.setup(1024, 768)
@@ -37,8 +35,6 @@ def prepare_turtle_canvas():
     turtle.onkey(stop, 'Escape')
     turtle.listen()
 
-
-
 def draw_big_point(p):
     turtle.goto(p)
     turtle.color(0.8, 0.9, 0)
@@ -50,13 +46,20 @@ def draw_point(p):
     turtle.goto(p)
     turtle.dot(5, random.random(), random.random(), random.random())
 
-
-
-
 def draw_curve_3_points(p1, p2, p3):
     # fill here
-    pass
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
 
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
+        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
+        draw_point((x, y))
+
+    draw_point(p3)
+    pass
 
 def draw_curve_4_points(p1, p2, p3, p4):
     draw_big_point(p1)
@@ -88,14 +91,14 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
-
-
-
-
 prepare_turtle_canvas()
 
+#draw_curve_3_points((-350, -100), (-50, 150), (150, -100))
+#draw_curve_3_points((-50, 150), (150, -100), (-350, -100))
 
+#draw_curve_3_points((-350, -100), (-50, 200), (150, -100))
+#draw_curve_3_points((-50, 200), (150, -100), (350, 300))
 
-
+draw_curve_4_points((-350, -100), (-50, 200), (150, -100), (350, 300))
 
 turtle.done()
