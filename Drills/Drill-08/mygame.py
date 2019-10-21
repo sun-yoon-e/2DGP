1767f82604c1d9e0a2b052727732bf6c1191b89b
sun-yoon-e <shinee252525@icloud.com>
2019-10-21 18:57:41 +0900

    Drill-08
    2018180018 박선윤
    Drill-08_1 https://youtu.be/jbNyQA_Jpck
    Drill-08_2 https://youtu.be/tl5DiqE3ZKs
    커밋횟수 : 2

---
+++ Drills/Drill-08/mygame.py
@@ -0,0 +1,10 @@
+import game_framework
+
+# fill here
+import pico2d
+
+import start_state
+
+pico2d.open_canvas()
+game_framework.run(start_state)
+pico2d.close_canvas()
---
+++ Drills/Drill-08/pause_advanced.py
@@ -0,0 +1,54 @@
+from pico2d import *
+
+import game_framework
+import main_state
+
+class Pause_Advanced:
+    def __init__(self):
+        self.image = load_image('pause_.png')
+
+    def update(self):
+        delay(0.1)
+        pass
+
+    def draw(self):
+        self.image.draw(400, 300, 400, 400)
+
+
+def enter():
+    global pause
+    pause = Pause_Advanced()
+
+
+def exit():
+    global pause
+    del pause
+
+
+def update():
+    global pause
+    pause.update()
+
+
+def draw():
+    global pause
+    clear_canvas()
+    main_state.draw()
+    pause.update()
+    pause.draw()
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
