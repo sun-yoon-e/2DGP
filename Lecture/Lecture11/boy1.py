78b1af4603c1eead57687e76d016983f9638128c
sun-yoon-e <shinee252525@icloud.com>
2019-10-28 19:08:30 +0900

    실습

--- Lecture/Lecture11/boy1.py
+++ Lecture/Lecture11/boy1.py
@@ -1,20 +1,24 @@
 from pico2d import *
 
 # Boy Event
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER, SHIFT_DOWN, SHIFT_UP = range(7)
 # fill here
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER = range(5)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
     (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
-    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYDOWN, SDLK_LSHIFT): SHIFT_DOWN,
+    (SDL_KEYDOWN, SDLK_RSHIFT): SHIFT_DOWN,
+    (SDL_KEYUP, SDLK_LSHIFT): SHIFT_UP,
+    (SDL_KEYUP, SDLK_RSHIFT): SHIFT_UP,
+
 }
 
-
 # Boy States
-
 # fill here
+
 
 class IdleState:
     @staticmethod
@@ -43,7 +47,7 @@ class IdleState:
     @staticmethod
     def draw(boy):
         if boy.dir == 1:
-            boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
+            boy.image.clip_draw(boy.frame * 100, 200, 100, 100 ,boy.x, boy.y)
         else:
             boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
 
@@ -98,21 +102,50 @@ class SleepState:
         if boy.dir == 1:
             boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, 3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
         else:
-            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, -3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
 
 
-# Dictionary
+class DashState:
+    @staticmethod
+    def enter(boy, event):
+        if boy.timer < 0:
+            boy.timer = 100
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.timer -= 1
+        boy.x += boy.velocity * 2
+        boy.x = clamp(25, boy.x, 800 - 25)
+        if boy.timer == 0:
+            boy.add_event(SHIFT_UP)
+
+    @staticmethod
+    def draw(boy):
+        if boy.velocity == 1:
+            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+
+
 next_state_table = {
-    # fill here
+# fill here
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
                 RIGHT_DOWN: RunState, LEFT_DOWN: RunState,
-                SLEEP_TIMER: SleepState},
-
+                SLEEP_TIMER: SleepState, SHIFT_DOWN: IdleState, SHIFT_UP: IdleState},
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
-               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState},
-
+               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
+               SHIFT_DOWN: DashState, SHIFT_UP:RunState},
     SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
-                 LEFT_UP: RunState, RIGHT_UP: RunState},
+                 LEFT_UP: RunState, RIGHT_UP: RunState,
+                 SHIFT_DOWN: SleepState, SHIFT_UP: SleepState},
+    DashState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
+                LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
+                SHIFT_DOWN: DashState, SHIFT_UP: RunState},
 }
 
 
@@ -130,7 +163,7 @@ class Boy:
         self.cur_state.enter(self, None)
         pass
 
-    def change_state(self, state):
+    def change_state(self,  state):
         # fill here
         if len(self.event_que) > 0:
             event = self.event_que.pop()
--- Lecture/Lecture11/main_state.py
+++ Lecture/Lecture11/main_state.py
@@ -6,11 +6,8 @@ from pico2d import *
 
 import game_framework
 
-
 from boy import Boy
 from grass import Grass
-
-
 
 name = "MainState"
 
