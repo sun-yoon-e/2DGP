ce488807f7546f49e289a9570301562c6ceb1d75
sun-yoon-e <shinee252525@icloud.com>
2019-11-12 21:15:09 +0900

    Drill-10
    2018180018 박선윤
    커밋횟수 : 6
    https://youtu.be/W8vksCYjAm4

--- Drills/Drill-10/bird.py
+++ Drills/Drill-10/bird.py
@@ -11,7 +11,7 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-TIME_PER_ACTION = 0.5
+TIME_PER_ACTION = 1.0
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 14
 
@@ -30,12 +30,12 @@ class Bird:
 
     def update(self):
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
-        self.frame_x = self.frame % 5
-        if self.frame % 14 == 0:
+        self.frame_x = int(self.frame) % 5
+        if int(self.frame) % 14 == 0:
             self.frame_y = 2
-        elif self.frame % 14 == 5:
+        if int(self.frame) % 14 == 5:
             self.frame_y = 1
-        elif self.frame % 14 == 10:
+        if int(self.frame) % 14 == 10:
             self.frame_y = 0
 
         self.x += self.velocity * game_framework.frame_time
@@ -48,6 +48,6 @@ class Bird:
 
     def draw(self):
         if self.dir == -1:
-            self.image.clip_composite_draw(int(self.frame_x) * 182, int(self.frame_y) * 168, 183, 168, 0.0, 'h', self.x, self.y, 183, 169)
+            self.image.clip_composite_draw(int(self.frame_x) * 182, int(self.frame_y) * 168, 182, 168, 0.0, 'h', self.x, self.y, 182, 168)
         elif self.dir == 1:
-            self.image.clip_draw(int(self.frame_x) * 182, int(self.frame_y) * 168, 183, 168, self.x, self.y, 183, 169)
+            self.image.clip_draw(int(self.frame_x) * 182, int(self.frame_y) * 168, 182, 168, self.x, self.y, 182, 168)
