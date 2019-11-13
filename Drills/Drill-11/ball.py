a8a00a8c88bee163531579df8d8ddc858878793c
sun-yoon-e <shinee252525@icloud.com>
2019-11-14 00:27:23 +0900

    Drill-11

--- Drills/Drill-11/ball.py
+++ Drills/Drill-11/ball.py
@@ -2,6 +2,8 @@ import random
 from pico2d import *
 import game_world
 import game_framework
+import main_state
+
 
 class Ball:
     image = None
@@ -10,6 +12,10 @@ class Ball:
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
         self.x, self.y, self.fall_speed = random.randint(0, 1600-1), 60, 0
+        self.plus_x = 0
+        self.brick = main_state.brick
+        self.collide = False
+        self.init = False
 
     def get_bb(self):
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
@@ -19,15 +25,19 @@ class Ball:
         draw_rectangle(*self.get_bb())
 
     def update(self):
-        self.y -= self.fall_speed * game_framework.frame_time
+        if self.collide == False:
+            self.y -= self.fall_speed * game_framework.frame_time
+        if self.collide and self.init:
+            self.x = self.brick.x + self.plus_x
+            self.y = self.brick.y + 40
 
     def stop(self):
         self.fall_speed = 0
 
 
 class BigBall(Ball):
-    MIN_FALL_SPEED = 50  # 50 pps = 1.5 meter per sec
-    MAX_FALL_SPEED = 200 # 200 pps = 6 meter per sec
+    MIN_FALL_SPEED = 50   # 50 pps = 1.5 meter per sec
+    MAX_FALL_SPEED = 200  # 200 pps = 6 meter per sec
     image = None
 
     def __init__(self):
@@ -35,6 +45,9 @@ class BigBall(Ball):
             BigBall.image = load_image('ball41x41.png')
         self.x, self.y = random.randint(0, 1600-1), 500
         self.fall_speed = random.randint(BigBall.MIN_FALL_SPEED, BigBall.MAX_FALL_SPEED)
+        self.brick = main_state.brick
+        self.collide = False
+        self.init = False
 
     def get_bb(self):
         return self.x - 20, self.y - 20, self.x + 20, self.y + 20
--- Drills/Drill-11/brick.py
+++ Drills/Drill-11/brick.py
@@ -7,12 +7,19 @@ class Brick:
     def __init__(self):
         self.image = load_image('brick180x40.png')
         self.x, self.y = 1600 // 2, 200
+        self.dir = 1
+        self.speed = 200
 
     def update(self):
-        
+        self.x += self.dir * self.speed * game_framework.frame_time
+
+        if self.x <= 0 + 90:
+            self.dir = 1
+        elif self.x >= 1600 - 90:
+            self.dir = -1
 
     def draw(self):
-        self.image.draw(500, 200)
+        self.image.draw(self.x, self.y)
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
